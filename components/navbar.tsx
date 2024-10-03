c027e79c63c20d243e3162d9f01ec9441755666f
BizTecBritain <biztecbritain@gmail.com>
2024-10-03 17:33:27 +0100

    Fixed styling errors caused by authz changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,16 +14,34 @@ import { Link } from "@nextui-org/link";
 import NextLink from "next/link";
 import clsx from "clsx";
 import { signOut, useSession } from "next-auth/react";
+import { useReducer } from "react";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useReducer((current) => !current, false);
+
   const pathname = usePathname();
   const session = useSession();
 
+  var navItems = siteConfig.navItems;
+
+  if (session.status != "authenticated") {
+    navItems = navItems.filter((route) => !route.requires.includes("login"));
+  }
+
+  if (!session.data?.user.admin) {
+    navItems = navItems.filter((route) => !route.requires.includes("admin"));
+  }
+
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar
+      isMenuOpen={isMenuOpen}
+      maxWidth="xl"
+      position="sticky"
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
@@ -31,27 +49,25 @@ export const Navbar = () => {
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <Link
+                className={clsx({
+                  ["pointer-events-none"]: pathname === item.href,
+                })}
+                color={pathname === item.href ? "primary" : "foreground"}
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </NavbarItem>
+          ))}
           {session.status == "authenticated" ? (
-            <>
-              {siteConfig.navItems.map((item) => (
-                <NavbarItem key={item.href}>
-                  <Link
-                    className={clsx({
-                      ["pointer-events-none"]: pathname === item.href,
-                    })}
-                    color={pathname === item.href ? "primary" : "foreground"}
-                    href={item.href}
-                  >
-                    {item.label}
-                  </Link>
-                </NavbarItem>
-              ))}
-              <NavbarItem>
-                <Link color="danger" onClick={async () => await signOut()}>
-                  Logout
-                </Link>
-              </NavbarItem>
-            </>
+            <NavbarItem>
+              <Link color="danger" onClick={async () => await signOut()}>
+                Logout
+              </Link>
+            </NavbarItem>
           ) : (
             <NavbarItem>
               <Link color="primary" href="/login">
@@ -78,35 +94,35 @@ export const Navbar = () => {
 
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
+          {navItems.map((item, index) => (
+            <NavbarMenuItem
+              key={`${item}-${index}`}
+              isActive={pathname === item.href}
+            >
+              <Link
+                className={clsx({
+                  ["pointer-events-none"]: pathname === item.href,
+                })}
+                color={pathname === item.href ? "primary" : "foreground"}
+                href={item.href}
+                size="lg"
+                onPress={setIsMenuOpen}
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
           {session.status == "authenticated" ? (
-            <>
-              {siteConfig.navItems.map((item, index) => (
-                <NavbarMenuItem
-                  key={`${item}-${index}`}
-                  isActive={pathname === item.href}
-                >
-                  <Link
-                    className={clsx({
-                      ["pointer-events-none"]: pathname === item.href,
-                    })}
-                    color={pathname === item.href ? "primary" : "foreground"}
-                    href={item.href}
-                    size="lg"
-                  >
-                    {item.label}
-                  </Link>
-                </NavbarMenuItem>
-              ))}
-              <NavbarItem>
-                <Link
-                  color="danger"
-                  size="lg"
-                  onClick={async () => await signOut()}
-                >
-                  Logout
-                </Link>
-              </NavbarItem>
-            </>
+            <NavbarItem>
+              <Link
+                color="danger"
+                size="lg"
+                onClick={async () => await signOut()}
+                onPress={setIsMenuOpen}
+              >
+                Logout
+              </Link>
+            </NavbarItem>
           ) : (
             <NavbarItem>
               <Link color="primary" href="/login" size="lg">
